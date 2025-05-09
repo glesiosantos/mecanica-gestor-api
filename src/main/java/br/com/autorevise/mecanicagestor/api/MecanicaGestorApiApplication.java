aae835037d596e7cef7035ed28291309a4aec331
Glêsio Santos <glesioss@gmail.com>
2025-05-09 12:41:21 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/autorevise/mecanicagestor/api/MecanicaGestorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.autorevise.mecanicagestor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecanicaGestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecanicaGestorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autorevise/mecanicagestor/api/MecanicaGestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autorevise.mecanicagestor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecanicaGestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
