cc04fde8696a08091345e5daa5eaa32c961759a3
Glêsio Santos <glesioss@gmail.com>
2025-05-09 18:00:33 -0300

    ajuste das variaveis de ambiente e colocando os allowed origins em lista

--- src/main/java/br/com/autorevise/mecanicagestor/api/config/CorsConfig.java
+++ src/main/java/br/com/autorevise/mecanicagestor/api/config/CorsConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
-    @Value("${api.urls.consumers}")
-    private String consumidor;
+    @Value("${api.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -18,7 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(consumidor)
+                        .allowedOrigins(allowedOrigins.toArray(new String[0]))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                                 "Access-Control-Request-Headers", "Authorization")
